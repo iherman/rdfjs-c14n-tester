5245047896de809ca65eb809b02b23d1e155410a
Ivan Herman <ivan@ivan-herman.net>
2023-01-25 16:31:10 +0100

    First version of the testing module.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # rdfjs-c14n Tester
-Testing the rdfjs-c14n
+Testing [rdfjs-c14n](https://github.com/iherman/rdfjs-c14n)
 
-[rdfjs-c14n](https://github.com/iherman/rdfjs-c14n) is an implementation (under development) of the [RDF Dataset Canonicalization](https://www.w3.org/TR/rdf-canon/) algorithm. (The algorithm is being specified by the W3C [RDF Dataset Canonicalization and Hash Working Group](https://www.w3.org/groups/wg/rch)). This repository is to become an "official" test for the algorithm, eventually relying on the testing methodology the WG will adopt in due time.
+[rdfjs-c14n](https://github.com/iherman/rdfjs-c14n) is an implementation (under development) of the [RDF Dataset Canonicalization](https://www.w3.org/TR/rdf-canon/) algorithm. (The algorithm is being specified by the W3C [RDF Dataset Canonicalization and Hash Working Group](https://www.w3.org/groups/wg/rch)). This repository is to become an "official" test for the algorithm, eventually relying on the testing methodology the WG will adopt in due time. The test material (test manifest and the tests themselves) are directly fetched from the [official repository](https://github.com/w3c/rdf-canon/) of the specification.
 
-Having this as a separate repository makes it possible to also test `rdfjs-c14n` as a separate module to be used, via npm, as an imported module. Note, however, that at this point the `package.json` file in the package relies on the local linkage to the `rdfjs-c14n` module rather than importing from npm. When the project gets to a final equilibrium point, the package file will be modified...
+Having this as a separate repository makes it also possible to test `rdfjs-c14n` as a separate module to be used, via `npm`, as an imported module in other places. 
 
+(Note, however, that at this point the `package.json` file in the package relies on the local linkage to the `rdfjs-c14n` module rather than importing from `npm`. When the project gets to a final equilibrium point, the package file will be modified. In the meantime, users of this module must modify the `package.json` file by putting a reference to `rdfjs-c14n` manually.)
 
+---
+Maintainer: [@iherman](https://github.com/iherman)
--- main.ts
+++ main.ts
@@ -1,14 +1,133 @@
+/**
+ * Testing the [rdfjs-c14n](https://github.com/iherman/rdfjs-c14n) implementation.
+ * 
+ * @packageDocumentation
+ */
+import * as utils                                   from './lib/utils';
+import { RDFCanon, YamlLogger, LogLevels, Logger }  from 'rdfjs-c14n';
+import { TestEntry, TestResult, Constants }         from './lib/types';
+import { Command }                                  from 'commander';
 
+
+/**
+ * Main entry point.
+ * 
+ * ```
+ * Usage: main [number] [options]
+ *
+ * Run a specific test from the test suite.
+ * 
+ * Options:
+ *   -f --full             Run the full tests suite, returns the list of fails (if any)
+ *   -n --number [number]  Test number
+ *   -d --debug            Display all log
+ *   -t --trace            Display trace log
+ *   -h, --help            display help for command
+ * ```
+ * 
+ * @async
+ */
 async function main(): Promise<void> {
-    const get_nq = async (test: string): Promise<string> => {
-        const nqp = await fetch(`https://raw.githubusercontent.com/w3c/rdf-canon/main/tests/urdna2015/${test}`);
-        return nqp.text();
-    }
+    // Minor thingy: the test numbers are always three digit, which may be a pain for the users to type...
+    const testNumber = (num?: string): string => {
+        if (num) {
+            switch (num.length) {
+                case 1:
+                    return `00${num}`;
+                case 2:
+                    return `0${num}`;
+                case 3:
+                default:
+                    return num;
+            }
+        } else {
+            return "002"
+        }
+    };
 
-    const test: string = "test002-in.nq";
-    console.log(`Fetching ${test}`);
-    const nq = await get_nq(test);
-    console.log(nq);
+    // This is thing we are testing...
+    const canonicalizer = new RDFCanon();
+
+    // Grab the list of official test references from the github repository, via the test manifest.
+    const tests:  TestEntry[] = await utils.getTestList(Constants.MANIFEST_NAME);
+
+    // This is just standard UI handling...
+    const program = new Command();
+    program
+        .name ('main [number]')
+        .description('Run either a specific test from the test suite or the full test suite')
+        .usage('[options]')
+        .option('-f --full', 'Run the full tests suite, just return the list of fails')
+        .option('-n --number [number]', 'Test number')
+        .option('-d --debug', 'Display all log')
+        .option('-t --trace', 'Display trace and debug log')
+        .parse(process.argv);
+
+    const options = program.opts();
+    const debug = options.debug ? true : false;
+    const trace = options.trace ? true : false;
+    
+    // "full" means that all the tests must be performed. Otherwise a single test is run with, possibly,
+    // a detailed log.
+    if (options.full) {
+        const promises: Promise<TestResult>[] = tests.map((t: TestEntry): Promise<TestResult> => utils.singleTest(t,canonicalizer));
+        // This is a potentially problematic step in case one of the test 'fetch' leads to an exception. That can happen
+        // if the manifest is faulty and refers to a non-existing tests, or one of the tests is unreachable. At some
+        // point is may be worth making the testing more robust in this respect.
+        //
+        // Also, exceptions may be raised here if the canonicalization program itself raises an exception. All the more
+        // reason for handling the exception better...
+        const results: TestResult[] = await Promise.all(promises);
+
+        // In an ideal case, this array is empty :-)
+        const errors: TestResult[] = results.filter((t: TestResult): boolean => !t.pass);
+
+        if (errors.length === 0) {
+            console.log("All tests pass.");
+        } else {
+            const faulty_tests: string[] = errors.map((t: TestResult): string => t.id);
+            console.log(`Failed tests: ${faulty_tests}`);
+        }
+    } else {
+        const num = (program.args.length === 0) ? testNumber(options.number) : testNumber(program.args[0]);
+        // The user may provide some rubbish in lieu of a test number, so this must be
+        // handled somehow. Raising an exception is probably the best approach...
+        try {
+            const locateTestEntry = (num: string, the_tests: TestEntry[]): TestEntry => {
+                for (const entry of the_tests) {
+                    if (entry.id === `${Constants.ID_PREFIX}${num}`) {
+                        return entry;
+                    }
+                }
+                throw("Wrong test number number...");
+            };
+            const the_test = locateTestEntry(num,tests);
+
+            // Set up the logger, if requested
+            let logger : Logger|undefined = undefined;
+            const logLevel = (debug) ? LogLevels.debug : ((trace) ? LogLevels.info : undefined);
+
+            if (logLevel) {
+                logger = new YamlLogger(logLevel);
+                canonicalizer.setLogger(logger);
+            }
+
+            // The real action for canonicalization (which may also raise some exceptions)
+            const result: TestResult  = await utils.singleTest(the_test, canonicalizer);
+
+            // For now, just display the results. This may change later, if the
+            // exact reporting format will be settled.
+            console.log(JSON.stringify(result,null,4));
+            
+            if (logger) {
+                console.log("\n>>>> Log <<<<");
+                console.log(logger.log);
+            }
+
+        } catch(error) {
+            console.error(`Something went wrong: ${error.message}`);
+        }
+    }
 }
 
 main();
